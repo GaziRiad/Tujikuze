83828bd1528b1cde725e8292e191a172f364de78
GaziRiad <ouhibimustapha36@gmail.com>
2025-01-27 16:42:51 +0100

    getting started on the our offer page

--- nextjs-app/app/our-offer/page.jsx
+++ nextjs-app/app/our-offer/page.jsx
@@ -1,5 +1,29 @@
+import Footer from "@/components/Footer";
+import Image from "next/image";
 import React from "react";
 
+import imgLarge from "../../public/images/2.jpg";
+
 export default function page() {
-  return <div>page</div>;
+  return (
+    <>
+      <section className="mx-auto mb-8 mt-20 max-w-[1720px] px-4 lg:mb-48 lg:mt-56">
+        <p className="mb-8 text-lg lg:mb-24 lg:text-4xl">
+          Tujikuze began with a vision to connect Kenyan artisans with global
+          markets while preserving traditional craftsmanship. Rooted in the
+          meaning of “to grow together” in Kiswahili, our story is one of
+          collaboration, sustainability, and empowerment. Partnering with
+          visionary Kenyan organizations, we bring authentic, handcrafted
+          products to the world.
+        </p>
+        <Image
+          src={imgLarge}
+          alt="man crafting"
+          className="mb-14 ml-auto aspect-square w-full object-cover object-center lg:mb-48 lg:w-1/2"
+        />
+      </section>
+
+      <Footer />
+    </>
+  );
 }
--- nextjs-app/components/about/OurMission.jsx
+++ nextjs-app/components/about/OurMission.jsx
@@ -1,68 +1,3 @@
-// "use client";
-
-// import Image from "next/image";
-// import React, { useState } from "react";
-// import SectionHeading from "../SectionHeading";
-
-// import imgLarge from "../../public/images/2.jpg";
-// import DataList from "../DataList";
-
-// const DATALIST = [
-//   {
-//     title: "Production entities",
-//     data: "5",
-//   },
-//   {
-//     title: "Artisan Communities",
-//     data: "100",
-//   },
-//   {
-//     title: "Network of Artisans",
-//     data: "10",
-//   },
-// ];
-
-// export default function OurMission() {
-//   const [isOpen, setIsOpen] = useState(false);
-
-//   return (
-//     <section>
-//       <div className="mx-auto max-w-[1720px] px-4">
-//         <SectionHeading
-//           title="Our Mission"
-//           type="tab"
-//           state={isOpen}
-//           onClick={() => setIsOpen(!isOpen)}
-//         />
-//         {isOpen && (
-//           <>
-//             <p className="mb-8 text-lg lg:mb-24 lg:text-4xl">
-//               Tujikuze’s mission is to empower Kenyan artisan communities by
-//               connecting them with global markets seeking authentic, sustainable
-//               products. By fostering ethical production and preserving
-//               traditional craftsmanship, we create opportunities for growth and
-//               meaningful impact. Tujikuze’s mission is to empower Kenyan artisan
-//               communities by connecting them with global markets seeking
-//               authentic, sustainable products. By fostering ethical production
-//               and preserving traditional craftsmanship, we create opportunities
-//               for growth and meaningful impact.
-//             </p>
-
-//             <DataList items={DATALIST} />
-//           </>
-//         )}
-//       </div>
-//       {isOpen && (
-//         <Image
-//           src={imgLarge}
-//           alt="man crafting"
-//           className="mb-9 h-[512px] w-full object-cover object-center lg:mb-48 lg:h-[920px]"
-//         />
-//       )}
-//     </section>
-//   );
-// }
-
 "use client";
 
 import Image from "next/image";
@@ -92,15 +27,15 @@ export default function OurMission() {
     <TabSection title="Our Mission">
       <div className="mx-auto max-w-[1720px] px-4">
         <p className="mb-8 text-lg lg:mb-24 lg:text-4xl">
-          Tujikuze’s mission is to empower Kenyan artisan communities by //
-          connecting them with global markets seeking authentic, sustainable //
-          products. By fostering ethical production and preserving //
-          traditional craftsmanship, we create opportunities for growth and //
-          meaningful impact. Tujikuze’s mission is to empower Kenyan artisan //
-          communities by connecting them with global markets seeking //
-          authentic, sustainable products. By fostering ethical production //
-          and preserving traditional craftsmanship, we create opportunities //
-          for growth and meaningful impact. //{" "}
+          Tujikuze’s mission is to empower Kenyan artisan communities by
+          connecting them with global markets seeking authentic, sustainable
+          products. By fostering ethical production and preserving traditional
+          craftsmanship, we create opportunities for growth and meaningful
+          impact. Tujikuze’s mission is to empower Kenyan artisan communities by
+          connecting them with global markets seeking authentic, sustainable
+          products. By fostering ethical production and preserving traditional
+          craftsmanship, we create opportunities for growth and meaningful
+          impact.
         </p>
         <DataList items={DATALIST} />
       </div>
