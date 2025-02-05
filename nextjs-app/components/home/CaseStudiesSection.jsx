63754286b95422487e0daae8650c826c88642a11
GaziRiad <ouhibimustapha36@gmail.com>
2025-02-05 03:08:32 +0100

    Fixed layout issue in home page

--- nextjs-app/components/home/CaseStudiesSection.jsx
+++ nextjs-app/components/home/CaseStudiesSection.jsx
@@ -3,7 +3,7 @@ import ListItem from "../ListItem";
 
 function CaseStudiesSection({ data }) {
   return (
-    <section className="mx-auto mb-14 max-w-[1720px] px-4 lg:mb-48">
+    <section>
       {data?.sectionHeading && (
         <SectionHeading
           title={data.sectionHeading.title}
@@ -11,7 +11,7 @@ function CaseStudiesSection({ data }) {
           href={data.sectionHeading?.link?.linkUrl}
         />
       )}
-      <ul className="mt-8 flex flex-col divide-y divide-dark-600 lg:mt-24">
+      <ul className="mx-auto mb-14 mt-8 flex max-w-[1720px] flex-col divide-y divide-dark-600 px-4 lg:mb-48 lg:mt-24">
         {data.caseStudies.map((item, index) => (
           <ListItem key={index} item={item} />
         ))}
--- nextjs-app/components/home/MaterialsSection.jsx
+++ nextjs-app/components/home/MaterialsSection.jsx
@@ -64,44 +64,46 @@ function MaterialsSection({ data }) {
   };
 
   return (
-    <section className="mx-auto mb-28 max-w-[1720px] px-4 lg:mb-48">
+    <section>
       <SectionHeading
         title={data.sectionHeading.title}
         label={data.sectionHeading?.link?.label}
         href={data.sectionHeading?.link?.linkUrl}
       />
-      <h2 className="mb-11 max-w-[1320px] text-lg lg:mb-28 lg:text-4xl">
-        {data.mainText}
-      </h2>
-      <div className="mb-48 flex flex-col items-start justify-between gap-14 lg:flex-row lg:gap-0">
-        <ul className="flex flex-col gap-[6px]">
-          {data.items.map((item, index) => (
-            <li
-              key={index}
-              className="grid cursor-pointer grid-cols-[30fr_70fr] text-[#BABABA] transition-all hover:text-[#1A1A1A]"
-              onMouseEnter={() => handleHover(item.itemImage.url)}
-              onMouseLeave={() => handleHover(img1)}
-            >
-              <p className="font-secondary text-sm">{item.name}</p>
-              <p className="text-sm lg:text-lg">{item.description}</p>
-            </li>
-          ))}
-        </ul>
-        <div
-          className={`relative h-[320px] w-full overflow-hidden lg:h-[520px] lg:w-[560px] 2xl:h-[620px]`}
-        >
-          <Image
-            width={1200}
-            height={1200}
-            src={hoveredImage}
-            alt="Hovered craftsmanship"
-            className={`absolute inset-0 h-full w-full object-cover transition-opacity duration-300 ${
-              isFading ? "opacity-0" : "opacity-100"
-            }`}
-          />
+      <div className="mx-auto mb-28 max-w-[1720px] px-4 lg:mb-48">
+        <h2 className="mb-11 max-w-[1320px] text-lg lg:mb-28 lg:text-4xl">
+          {data.mainText}
+        </h2>
+        <div className="mb-48 flex flex-col items-start justify-between gap-14 lg:flex-row lg:gap-0">
+          <ul className="flex flex-col gap-[6px]">
+            {data.items.map((item, index) => (
+              <li
+                key={index}
+                className="grid cursor-pointer grid-cols-[30fr_70fr] text-[#BABABA] transition-all hover:text-[#1A1A1A]"
+                onMouseEnter={() => handleHover(item.itemImage.url)}
+                onMouseLeave={() => handleHover(img1)}
+              >
+                <p className="font-secondary text-sm">{item.name}</p>
+                <p className="text-sm lg:text-lg">{item.description}</p>
+              </li>
+            ))}
+          </ul>
+          <div
+            className={`relative h-[320px] w-full overflow-hidden lg:h-[520px] lg:w-[560px] 2xl:h-[620px]`}
+          >
+            <Image
+              width={1200}
+              height={1200}
+              src={hoveredImage}
+              alt="Hovered craftsmanship"
+              className={`absolute inset-0 h-full w-full object-cover transition-opacity duration-300 ${
+                isFading ? "opacity-0" : "opacity-100"
+              }`}
+            />
+          </div>
         </div>
+        <MaterialsCarousel items={data.materials} />
       </div>
-      <MaterialsCarousel items={data.materials} />
     </section>
   );
 }
